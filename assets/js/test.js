fdf192e861783a9ce5e011406804a0c9ba0566e2
[Andrew Core] <[adcore89@gmail.com]>
2021-04-04 17:36:47 -0400

    added Header tags for populated fields

--- assets/js/test.js
+++ assets/js/test.js
@@ -3,12 +3,19 @@ var inputValue = document.querySelector("#search-city");
 var confirmed = document.querySelector(".confirmed");
 var recovered = document.querySelector(".recovered");
 var deaths = document.querySelector(".deaths");
+var counName = document.querySelector("#country-search-term")
 
+
+//Function to call API
 
 function countrySearch(event) {
   event.preventDefault();
   console.log("click");
   var countryName = inputValue.value;
+
+// Need to Add Error checking
+
+// API fetch with countryName as dynamic user generated variable 
 
   fetch(`https://covid-api.mmediagroup.fr/v1/cases?country=${countryName}`)
   .then(function(response) { 
@@ -17,12 +24,15 @@ function countrySearch(event) {
       console.log(data);
 
     // get array for confirmed, recovered, deaths   
+    var counValue = countryName;  
     var allValue = data[`All`][`confirmed`]; 
     var recValue = data[`All`][`recovered`];
     var deathValue = data[`All`][`deaths`];
 
 
     //print data
+    
+    counName.innerHTML =  counValue
     confirmed.innerHTML = allValue;
     recovered.innerHTML = recValue;
     deaths.innerHTML = deathValue;
@@ -31,5 +41,6 @@ function countrySearch(event) {
     });
   });
 };
+
 
 button.addEventListener("click", countrySearch);
