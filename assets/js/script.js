c2d8e363dabb548f233c27be3fa0d179e79feb8f
Deepika Bekal <deepika.bekal2@gmail.com>
2021-04-04 16:19:50 +0400

    function for API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,19 @@ var apiUrl = `http://api.mediastack.com/v1/news`;//mediastack API URL
 var newsAbout = "covid coronavirus";//keywords for the search
 var queryPara = `?keywords=${newsAbout}&countries=${countryCode}&languages=en&sort=published_asc`;//query to get covid relted news associted with country code in ascending order
 
+//function to make an API call to get the data
+function makeApiCall(countryCode){
+
+    fetch(apiUrl+queryPara+`&access_key=${apiKey}`)
+    .then(function(reponse){
+        return reponse.json();
+    })
+    .then(function(data){
+        console.log(data);        
+        
+    })
+}
+
+makeApiCall(countryCode);
+
 
